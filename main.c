4add04d62ddeb04cb17e3129504247b1d5915b97
Ohnun <johnsun2019@qq.com>
2023-09-03 18:22:56 +0800

    Echo example

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
 int main(){
+	char *str = NULL; // Array to store the string.
+	int str_len = 0;  // Length of array.
+	char c;
+	// store input
+	while((c = getchar()) != '\n'){
+		str = (char*)realloc(str, (str_len + 1) * sizeof(char));
+		str[str_len++] = tolower(c);
+	}
+	// print string
+	for(int i = 0; i < str_len; i++){
+		printf("%c%s", str[i], i == str_len - 1 ? "\n" : "");
+	}
+
+	// release mem
+	free(str);
 	return 0;
 }
