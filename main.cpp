3f1085bf070c674f1c6c74fd6bc87039e9d20a0d
Ohnun <johnsun2019@qq.com>
2023-09-07 21:01:01 +0800

    use string

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ using std::endl;
 using std::string;
 
 int main(){
-	string line;
-	while(getline(cin, line)){
-		cout << line << endl;
+	const string hexdigits = "0123456789ABCDEF";
+	cout << "Enter a series of numbers between 0 and 15 separted by spaces." << endl;
+	cout << "Hit ENTER when finished: " << endl;
+	string result; // to restore hex string.
+	string::size_type n; // to store input number.
+
+	while(cin >> n){
+		if(n < hexdigits.size()) result += hexdigits[n];
 	}
+	cout << "Your hex number is: " << result << endl;
 
 	return 0;
 }
