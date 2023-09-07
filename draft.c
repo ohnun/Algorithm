c93848aa5ee0cbf9b8aa5d40ccbef706c550a800
Ohnun <johnsun2019@qq.com>
2023-09-07 18:07:49 +0800

    modify draft.c

--- draft.c
+++ draft.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    FILE *fp = fopen("main.c", "w");
+    // Change "main.c" to "main.cpp"
+    FILE *fp = fopen("main.cpp", "w");
 
     if(fp == NULL){
-        fprintf(stderr, "Can't create \"main.c\".\n");
+        fprintf(stderr, "Can't create \"main.cpp\".\n");
         exit(EXIT_FAILURE);
     }
     
-    fprintf(fp, "#include<stdio.h>\n");
+    // Add head files.
+    fprintf(fp, "#include<iostream>\n");
+    fprintf(fp, "#include<string>\n");
+    // Add using space.
+    fprintf(fp, "using std::cin;\n");
+    fprintf(fp, "using std::cout;\n");
+    fprintf(fp, "using std::endl;\n");
+    fprintf(fp, "using std::string;\n\n");
+    // body.
     fprintf(fp, "int main(){\n");
     fprintf(fp, "\treturn 0;\n");
     fprintf(fp, "}\n");
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<string>
+using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 int main(){
-    cout << "Hello, world!" << endl;
-
-    return 0;
+	return 0;
 }
