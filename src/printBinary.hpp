a16812fc6c71039c5ce61b3c4288fb566687b9ed
Ohnun <johnsun2019@qq.com>
2024-04-08 00:54:33 +0800

    creat printBinary

---
+++ src/printBinary.hpp
@@ -0,0 +1,12 @@
+#ifndef _PRINT_BINARY_HPP
+#define _PRINT_BINARY_HPP
+
+/*
+* Print binary format of numerical values. 
+* @version: 0.01
+* @date: 2024-4-7
+* @author: John Sun
+*/
+void printBinary(int);
+
+#endif
