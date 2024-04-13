df3ac35826fe7a33b9bed015585050034e683647
Ohnun <johnsun2019@qq.com>
2024-04-13 22:05:04 +0800

    record: leetCode 7

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <vector>
-#include "src/binarySearch.hpp"
-
+#include <string>
+using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
 
 int main(){
-	vector<int> arr{1, 3, 9, 11, 12, 43, 44, 45, 90, 99};
-	int r = findRight(arr, 8);
-	cout << r << endl;
 
 	return 0;
 }
