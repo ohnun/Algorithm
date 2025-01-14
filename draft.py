dfdf88ceb724c4903a983e2fcd5dafc508cd4c09
ohnun <johnsun2019@qq.com>
2025-01-15 07:18:40 +0800

    finish day 001

--- draft.py
+++ draft.py
@@ -21,7 +21,7 @@ java_tamplate = """\
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // System.out.println("Hello, world!");
     }
 }
 """
